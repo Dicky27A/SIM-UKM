2f5a28a314443ab8b198677d1efb116d6ad692ed
Dicky ADi Saputra <dickyumum27@gmail.com>
2025-01-14 15:25:20 +0700

    Edit Tabel

--- app/Models/pricing.php
+++ app/Models/pricing.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class pricing extends Model
 {
     //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name', // Web Design category name
+        'duration', // slug = web-design
+        'price', // slug = web-design
+    ];
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
